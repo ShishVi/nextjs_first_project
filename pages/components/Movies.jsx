e6046919896d8338f531a39c2eaf4748496ef4f5
Vitaly <shishov_84@mail.ru>
2023-02-05 21:08:30 +0300

    added component Movies

---
+++ pages/components/Movies.jsx
@@ -0,0 +1,10 @@
+export default function Movies (props) {
+
+    let hrefImbd = `https://www.imdb.com/title/${props.item.imdbID}`;
+    return <div>
+        <a href={hrefImbd} target="_blank">
+        <h2>{props.item.Title}({props.item.Year})</h2>
+        <img src = {props.item.Poster}  />
+        </a>
+    </div>
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,37 @@
+import {useState,useEffect} from 'react';
+import Movies from './components/Movies';
+
+
 export default function HomePage() {
+
+    const[text, setText] = useState("War");
+    const[arrMovies, setarrMovies] = useState([]);
+
+    useEffect(() => {
+        // declare the async data fetching function
+        const fetchData = async () => {
+          // get the data from the api
+          const data = await fetch("https://www.omdbapi.com/?apikey=a2b07930&s=" + text);
+          // convert the data to json
+          const json = await data.json();
+      
+          // set state with the result
+          setarrMovies(json.Search);
+        };
+      
+        // call the function
+        fetchData()
+          // make sure to catch any error
+          .catch(console.error);
+        console.log(text);
+
+      }, []);
+
+    console.log(arrMovies);
+
 return <>
-<h2>Home page!</h2>
+<input value={text} onInput ={evt=>setText(evt.target.value)}/>
+{arrMovies.map((item)=> <Movies item = {item} />)}
+
 </>
 }
